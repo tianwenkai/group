debb67c581b903eccc28f7d47fccc5718bf75029
tianwenkai <276552806@qq.com>
2020-03-24 11:01:10 +0800

    123

--- src/main/java/com/woniuxy/controller/EmpController.java
+++ src/main/java/com/woniuxy/controller/EmpController.java
@@ -18,7 +18,7 @@ public class EmpController {
     //展示所有员工信息，不带分页
     @RequestMapping("index")
     public String index(Model model) {
-        System.out.println("123");
+        
         List<Emp> empList = empService.findAll();
         model.addAttribute("empList", empList);
         for (Emp e:empList){
